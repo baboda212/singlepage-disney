8b9c63e55124ad740298df5f121110d0e9daa0c7
baboda212 <baboda212@naver.com>
2023-01-02 17:47:59 +0900

    header logo삽입

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
         <h1>
             <img src={`/images/logo-sm.png`} alt="타이틀이미지" />
         </h1>
-        <p>©Disney ©Disney/Pixar ©MARVEL © & ™ Lucasfilm Ltd. ©2021 20CS</p>
+        <p className="copy">©Disney ©Disney/Pixar ©MARVEL © & ™ Lucasfilm Ltd. ©2021 20CS</p>
+        <p className="address">회사소개 | 채용 | 이용약관 | 개인정보처리방침</p>
     </footer>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import myStyle from './Header.module.css';
 export default function Header() {
   return (
     <header>
-        <h1 className={myStyle.textPrimary}>디즈니 홈페이지</h1>
+        <h1 className={myStyle.textPrimary}><img src='/images/logo.png'/></h1>
         <nav>
-            <a href="#title">title</a>
+            <a href="#title">디즈니플러스</a>
             <a href="#disney">디즈니</a>
             <a href="#marvel">마블</a>
             <a href="#starwars">스타워즈</a>
             <a href="#NGG">네셔널지오그래픽</a>
-            <a href="#20th">20th century studios</a>
+            <a href="#20th">20세기폭스사</a>
         </nav>
     </header>
   )
